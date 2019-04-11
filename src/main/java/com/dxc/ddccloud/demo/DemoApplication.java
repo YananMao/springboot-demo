49aa7632f675a8f19d8fa69197e2ea9dabda73ad
yananmao <yananmao@hengtiansoft.com>
2019-04-11 12:30:46 +0800

    ceshi

--- src/main/java/com/dxc/ddccloud/demo/DemoApplication.java
+++ src/main/java/com/dxc/ddccloud/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.dxc.ddccloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
